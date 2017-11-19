98d1a607b199fbe4ae983bd752dc544b0f146d98
Michel Deiman <michel.deiman@live.nl>
2017-11-19 02:04:53 +0100

    Initial Commit

---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Michel Deiman on 17/11/2017.
+//  Copyright © 2017 Michel Deiman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+	var deck = PlayingCardDeck()
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+//		var card = deck.draw()
+//		while card != nil {
+//			print("\(card!)")
+//			card = deck.draw()
+//		}
+	}
+
+
+}
+
